78882f6f930b39032c7381ba769a0fdfc707b490
= <sasukegc@gmail.com>
2019-05-11 18:40:05 -0300

    Create list category with dto

--- src/main/java/com/velasco/ecommerceapi/resources/CategoryResource.java
+++ src/main/java/com/velasco/ecommerceapi/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.velasco.ecommerceapi.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.velasco.ecommerceapi.domain.Category;
+import com.velasco.ecommerceapi.dto.CategoryDTO;
 import com.velasco.ecommerceapi.services.CategoryService;
 
 @RestController
@@ -46,6 +49,13 @@ public class CategoryResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<Category> list = service.findAll();
+		List<CategoryDTO> listDTO = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 
