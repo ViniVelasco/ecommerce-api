dafc86d71a39553ce44dc7c7c720ea70f89a5af7
= <sasukegc@gmail.com>
2019-05-10 22:24:26 -0300

    create instantion operation

--- src/main/java/com/velasco/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/velasco/ecommerceapi/EcommerceApiApplication.java
@@ -1,13 +1,34 @@
 package com.velasco.ecommerceapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EcommerceApiApplication {
+import com.velasco.ecommerceapi.domain.Category;
+import com.velasco.ecommerceapi.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class EcommerceApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
