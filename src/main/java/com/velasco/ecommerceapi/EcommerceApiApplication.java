55c918b4fef28c930c656d851af67ddeb00f86ad
= <sasukegc@gmail.com>
2019-05-11 19:12:44 -0300

    Pagination with optional parameters in requisition

--- src/main/java/com/velasco/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/velasco/ecommerceapi/EcommerceApiApplication.java
@@ -71,12 +71,23 @@ public class EcommerceApiApplication implements CommandLineRunner {
 		
 		Category cat1 = new Category(null, "Informática");
 		Category cat2 = new Category(null, "Escritório");
+		Category cat3 = new Category(null, "Cama mesa e banho");
+		Category cat4 = new Category(null, "Eletrônicos");
+		Category cat5 = new Category(null, "Jardinagem");
+		Category cat6 = new Category(null, "Decoração");
+		Category cat7 = new Category(null, "Perfumaria");
 		
 		Product p1 = new Product(null, "Computador", 2000.00);
 		Product p2 = new Product(null, "Impressora", 800.00);
 		Product p3 = new Product(null, "Mouse", 80.00);
 		
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 		
 		State st1 = new State(null, "Minas Gerais");
 		State st2 = new State(null, "São Paulo");
@@ -87,12 +98,6 @@ public class EcommerceApiApplication implements CommandLineRunner {
 		
 		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProducts().addAll(Arrays.asList(p2));
-		
-		p1.getCategories().addAll(Arrays.asList(cat1));
-		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategories().addAll(Arrays.asList(cat1));
-		
-		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		st1.getCities().addAll(Arrays.asList(cit1));
 		st2.getCities().addAll(Arrays.asList(cit2, cit3));
