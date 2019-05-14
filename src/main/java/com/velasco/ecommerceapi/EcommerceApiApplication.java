f6e39de458264795ef8bd009101ae90f667f7ec3
Vinicius Velasco <viniciusvelasco@daxplace.com>
2019-05-14 14:03:36 -0300

    Upload in S3

--- src/main/java/com/velasco/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/velasco/ecommerceapi/EcommerceApiApplication.java
@@ -1,20 +1,25 @@
 package com.velasco.ecommerceapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.velasco.ecommerceapi.services.S3Service;
+
 @SpringBootApplication
 public class EcommerceApiApplication implements CommandLineRunner {
 
-
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApiApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("C:\\global.jpg");
 	}
 
 }
