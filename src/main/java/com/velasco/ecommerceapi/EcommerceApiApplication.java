68e205be66bb3f030f627033060b65137ea309ac
Vinicius Velasco <viniciusvelasco@daxplace.com>
2019-05-14 14:54:11 -0300

    Sending image via endpoint

--- src/main/java/com/velasco/ecommerceapi/EcommerceApiApplication.java
+++ src/main/java/com/velasco/ecommerceapi/EcommerceApiApplication.java
@@ -1,17 +1,11 @@
 package com.velasco.ecommerceapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.velasco.ecommerceapi.services.S3Service;
-
 @SpringBootApplication
 public class EcommerceApiApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApiApplication.class, args);
@@ -19,7 +13,6 @@ public class EcommerceApiApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\global.jpg");
 	}
 
 }
--- src/main/java/com/velasco/ecommerceapi/services/ClientService.java
+++ src/main/java/com/velasco/ecommerceapi/services/ClientService.java
@@ -1,5 +1,6 @@
 package com.velasco.ecommerceapi.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.velasco.ecommerceapi.domain.Address;
 import com.velasco.ecommerceapi.domain.City;
@@ -36,6 +38,9 @@ public class ClientService {
 	
 	@Autowired
 	private AddressRepository addressRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Client find(Integer id) {
 		
@@ -103,6 +108,10 @@ public class ClientService {
 	private void updateData(Client newObj, Client obj) {
 		newObj.setName(obj.getName());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return  s3Service.uploadFile(multipartFile);
 	}
 	
 }
